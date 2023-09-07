ef9747c5d63b6421c4b9b8161fb32df50ad473ed
Jarvisgame <912259240@163.com>
2023-09-07 15:59:50 +0800

    feat: 初始化页面

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <ClientOnly>
+      <NuxtPage></NuxtPage>
+    </ClientOnly>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    这里是index页面
+  </div>
+</template>
